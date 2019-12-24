06608a80f43958392545365f49853bd0dcf359c8
k-shimari <k-simari@ist.osaka-u.ac.jp>
2019-12-24 16:21:43 +0900

    add printjson

--- src/ncdsearch/clustering/debug/OutputClusters.java
+++ src/ncdsearch/clustering/debug/OutputClusters.java
@@ -2,12 +2,17 @@ package ncdsearch.clustering.debug;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import ncdsearch.clustering.Clusters;
 import ncdsearch.clustering.JsonNodesInfo;
@@ -93,6 +98,64 @@ public class OutputClusters {
 		}
 	}
 
+	public void outputJson(int ID, String dir) {
+
+		//List<List<JsonNode>> jsonList = new ArrayList<>(clusters.getClusterReps());
+		int index[] = new int[1];
+		index[0] = 1;
+		for (List<JsonNode> list : clusters.getClusterReps()) {
+			clusters.getRepJsonMap().get(list.get(0)).forEach(node -> {
+				((ObjectNode) node).put("clusterID", index[0]);
+			});
+			index[0]++;
+		}
+
+		ResultJson rj = new ResultJson(clusters.getAllNode());
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			List<String> lines = new ArrayList<>();
+			lines.add(mapper.writeValueAsString((Object) rj));
+			Path path = Paths.get(dir, "clustering_result", "result-" + ID + ".json");
+			if (Files.exists(path)) {
+				Files.delete(path);
+			}
+			Files.createFile(path);
+			Files.write(path, lines, Charset.forName("UTF-8"),
+					StandardOpenOption.WRITE);
+
+			//
+		} catch (Exception e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		//			System.out.println(jsonList.toString());
+		//			for (List<JsonNode> list : jsonList) {
+		//				clusters.getRepJsonMap().get(list.get(0)).forEach(s -> {
+		//					try {
+		//						bw.write(s.toString());
+		//						//bw.write(s.get("Tokens").asText());
+		//						bw.write("\n");
+		//					} catch (IOException e) {
+		//						// TODO 自動生成された catch ブロック
+		//						e.printStackTrace();
+		//					}
+		//				});
+		//			}
+
+	}
+
+	public class ResultJson {
+		List<JsonNode> Result;
+
+		public ResultJson(List<JsonNode> Result) {
+			this.Result = Result;
+		}
+
+		public List<JsonNode> getResult() {
+			return Result;
+		}
+	}
 
 
 }
