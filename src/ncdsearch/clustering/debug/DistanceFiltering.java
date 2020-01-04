e5a5838b293da68f797f64e2c6131561e823553a
k-shimari <k-simari@ist.osaka-u.ac.jp>
2020-01-04 15:38:02 +0900

    add DistanceFiltering

---
+++ src/ncdsearch/clustering/debug/DistanceFiltering.java
@@ -0,0 +1,78 @@
+package ncdsearch.clustering.debug;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import gnu.trove.map.hash.TIntDoubleHashMap;
+import ncdsearch.clustering.JsonNodeInfo;
+import ncdsearch.clustering.strategy.Cluster;
+import ncdsearch.clustering.strategy.Component;
+import ncdsearch.clustering.strategy.DistanceClustering;
+
+public class DistanceFiltering extends DistanceClustering {
+	private double clusterDistance;
+
+	public DistanceFiltering(List<JsonNode> allNode, String strategy, int clusterNum, double exDistanceThreshold,
+			double clusterDistance) {
+		super(allNode, strategy, clusterNum, exDistanceThreshold);
+		this.clusterDistance = clusterDistance;
+	}
+
+	@Override
+	public List<List<JsonNode>> clustering() {
+		init();
+		List<Cluster> allClusterList = new ArrayList<>();
+		for (int i = 0; i < totalVertexNumber; i++) {
+			for (Component co : clusterMap.get(i).getComponents()) {
+				if (JsonNodeInfo.getNodeDistance(co.getJsonNode()) <= exDistanceThreshold) {
+					allClusterList.add(clusterMap.get(i));
+					removedFlagMap[i] = true;
+				}
+			}
+		}
+
+		for (int i = 0; i < totalVertexNumber; i++) {
+			if (removedFlagMap[i])
+				continue;
+			TIntDoubleHashMap innerMap = distanceMap.get(i);
+			for (int j = 0; j < totalVertexNumber; j++) {
+				if (i != j) {
+					double distance = innerMap.get(j);
+					//need additional param
+					if (distance <= clusterDistance) {
+						allClusterList.add(clusterMap.get(i));
+						removedFlagMap[j] = true;
+					}
+				}
+			}
+		}
+
+		List<List<JsonNode>> nodeList = new ArrayList<>();
+		for (int i = 0; i < totalVertexNumber; i++) {
+			if (removedFlagMap[i]) {
+				for (Component co : clusterMap.get(i).getComponents()) {
+					List<JsonNode> list = new ArrayList<>();
+					list.add(co.getJsonNode());
+					nodeList.add(list);
+				}
+			}
+		}
+		return nodeList;
+	}
+
+	public List<List<JsonNode>> exClustering() {
+		return clustering();
+	}
+
+	@Override
+	protected void update() {
+
+	}
+
+	@Override
+	protected double calcDistance(Cluster c1, Cluster c2) {
+		return 0;
+	}
+}
